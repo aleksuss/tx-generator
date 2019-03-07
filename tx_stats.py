322767f85d34c51add5983d4370c3ad4be0aadbb
atrykush <andrii.trykush@gmail.com>
2019-03-07 16:57:25 +0200

    added pushgateway export; added ability to run as a service; added argparse to parse cli arguments

--- tx_stats.py
+++ tx_stats.py
@@ -7,18 +7,17 @@ import requests
 import sys
 from datetime import datetime
 from time import sleep
+from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
+import argparse
+from urllib.parse import urlparse
 
 count_blocks = 10
 
-
-def get_hostname():
-    if len(sys.argv) < 2:
-        print("Provide a hostname of the node")
-        exit(1)
-    if "http" in sys.argv[1]:
-        return sys.argv[1]
+def get_hostname(h):
+    if "http" in h:
+        return h
     else:
-        return "http://" + sys.argv[1]
+        return "http://" + h
 
 
 def parse_datetime(d_time):
@@ -73,12 +72,34 @@ def calc_current_tps(data):
 
 
 def main():
-    hostname = get_hostname()
+    parser = argparse.ArgumentParser(description='Exonum node\'s TPS stats collector')
+    parser.add_argument('-s', '--service', action='store_true', help='Run as a system service and export metrics to Prometheus')
+    parser.add_argument('-n', '--node', type=str, help='Exonum node\'s address', required=True)
+    parser.add_argument('-p', '--pushgateway', nargs=1, type=str, help='Prometheus pushgateway address')
+
+    args = parser.parse_args()
+
+    hostname = get_hostname(args.node)
+
     blocks_url = "{}/api/explorer/v1/blocks?count={}&add_blocks_time=true".format(
         hostname, count_blocks
     )
     stats = dict()
-    print("TPS statistics for host: %s" % hostname)
+
+    if (args.service and not args.pushgateway):
+        print('Pushgateway address required in service mode')
+        exit(1)
+
+    if (args.pushgateway):
+        registry = CollectorRegistry()
+        grouping_keys = {}
+        metric_current_tps_name = 'exonum_node_tps_current'
+        metric_avg_tps_name = 'exonum_node_tps_average'
+        metric_current_height_name = 'exonum_node_current_height'
+        metric_avg_tps = Gauge(metric_avg_tps_name, 'Exonum\'s node average TPS', registry=registry)
+        metric_current_height = Gauge(metric_current_height_name, 'Exonum\'s node current height', registry=registry)
+        metric_current_tps = Gauge(metric_current_tps_name, 'Exonum\'s node current TPS', registry=registry)
+        grouping_keys['instance'] = urlparse(hostname).netloc
 
     while True:
         try:
@@ -92,12 +113,21 @@ def main():
                 avrg_tps = calc_average_tps(stats)
                 current_tps = calc_current_tps(data)
                 last_height = int(data["range"]["end"])
-                print(
-                    "min: {}, max: {}, avrg: {}, current: {}, last height: {}".format(
-                        min_tps, max_tps, avrg_tps, current_tps, last_height
-                    ),
-                    end="\r",
-                )
+                if (args.pushgateway):
+                    try:
+                        metric_avg_tps.set(avrg_tps)
+                        metric_current_tps.set(current_tps)
+                        metric_current_height.set(last_height)
+                        push_to_gateway(args.pushgateway[0], job='StressTesting', registry=registry, grouping_key=grouping_keys)
+                    except Exception as e:
+                        print('Cannot send to prometheus: {}'.format(e))
+                if (not args.service):
+                    print(
+                        "min: {}, max: {}, avrg: {}, current: {}, last height: {}".format(
+                            min_tps, max_tps, avrg_tps, current_tps, last_height
+                        ),
+                        end="\r",
+                    )
             else:
                 print("Bad request")
 
@@ -110,3 +140,32 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# [Unit]
+# Description=Blockchain heights monitoring
+# Wants=network-online.target
+# After=network-online.target
+
+# [Service]
+# User=prometheus
+# Group=prometheus
+# Type=simple
+# ExecStart=/usr/local/bin/heights.py
+
+# [Install]
+# WantedBy=multi-user.target
+
+# [Unit]
+# Description=Prometheus Push Gateway
+# Wants=network-online.target
+# After=network-online.target
+
+# [Service]
+# User=prometheus
+# Group=prometheus
+# Type=simple
+# ExecStart=/usr/local/bin/pushgateway \
+#     --persistence.file /var/lib/pushgateway/metrics.log
+
+# [Install]
+# WantedBy=multi-user.target
