27edc87e312ead30fb7ea05aee6e121748b25ad6
Oleksandr Anyshchenko <oleksandr.anyshchenko@xdev.re>
2019-03-07 10:24:14 +0200

    Adopted datetime parsing for linux and code formatting

--- tx_stats.py
+++ tx_stats.py
@@ -3,40 +3,44 @@
 # This scripts outputs TPS stats in runtime
 # Run example: ./tx_stats.py node.hostname.com:8080
 
-import sys
 import requests
-from time import sleep
+import sys
 from datetime import datetime
+from time import sleep
 
 count_blocks = 10
 
 
 def get_hostname():
     if len(sys.argv) < 2:
-        print('Provide a hostname of the node')
+        print("Provide a hostname of the node")
         exit(1)
-    if 'http' in sys.argv[1]:
+    if "http" in sys.argv[1]:
         return sys.argv[1]
     else:
-        return 'http://' + sys.argv[1]
+        return "http://" + sys.argv[1]
 
 
 def parse_datetime(d_time):
-    return datetime.strptime(d_time, '%Y-%m-%dT%H:%M:%S.%fZ')
+    return datetime.strptime(d_time[:-4], "%Y-%m-%dT%H:%M:%S.%f")
 
 
 def update_stats(stats, data):
-    times = data['times']
-    for i, block in enumerate(data['blocks']):
-        height = block['height']
-        tx_count = block['tx_count']
+    times = data["times"]
+    for i, block in enumerate(data["blocks"]):
+        height = block["height"]
+        tx_count = block["tx_count"]
         if height in stats:  # skip existence entries
             continue
         if tx_count == 0:  # skip blocks with no transactions
             continue
-        if i == len(data['blocks']) - 1:  # skip last block info, we won't be able to calculate time delta
+        if (
+            i == len(data["blocks"]) - 1
+        ):  # skip last block info, we won't be able to calculate time delta
             continue
-        block_time = (parse_datetime(times[i]) - parse_datetime(times[i + 1])).total_seconds()
+        block_time = (
+            parse_datetime(times[i]) - parse_datetime(times[i + 1])
+        ).total_seconds()
         stats[height] = tx_count / block_time
 
 
@@ -62,14 +66,16 @@ def calc_average_tps(stats):
 
 
 def calc_current_tps(data):
-    times = data['times']
+    times = data["times"]
     delta_time = parse_datetime(times[0]) - parse_datetime(times[1])
-    return int(data['blocks'][0]['tx_count'] / delta_time.total_seconds())
+    return int(data["blocks"][0]["tx_count"] / delta_time.total_seconds())
 
 
 def main():
     hostname = get_hostname()
-    blocks_url = '{}/api/explorer/v1/blocks?count={}&add_blocks_time=true'.format(hostname, count_blocks)
+    blocks_url = "{}/api/explorer/v1/blocks?count={}&add_blocks_time=true".format(
+        hostname, count_blocks
+    )
     stats = dict()
     print("TPS statistics for host: %s" % hostname)
 
@@ -84,11 +90,15 @@ def main():
                 max_tps = calc_max_tps(stats)
                 avrg_tps = calc_average_tps(stats)
                 current_tps = calc_current_tps(data)
-                last_height = int(data['range']['end'])
-                print("min: {}, max: {}, avrg: {}, current: {}, last height: {}"
-                      .format(min_tps, max_tps, avrg_tps, current_tps, last_height), end="\r")
+                last_height = int(data["range"]["end"])
+                print(
+                    "min: {}, max: {}, avrg: {}, current: {}, last height: {}".format(
+                        min_tps, max_tps, avrg_tps, current_tps, last_height
+                    ),
+                    end="\r",
+                )
             else:
-                print('Bad request')
+                print("Bad request")
 
             sleep(1)
 
@@ -97,5 +107,5 @@ def main():
             exit(0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
